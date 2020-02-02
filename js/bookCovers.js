25e63d4e002471d326116a7e7e03b3d2ef146676
Bernardo Ayala <bernardoayala2@gmail.com>
2020-02-02 17:09:17 -0400

    Small style modifications and favicon removed

--- js/bookCovers.js
+++ js/bookCovers.js
@@ -1,12 +1,10 @@
-***REMOVED***
 const url = "https://www.googleapis.com/books/v1/volumes?q=subject:";
 
-async function getBooks(url, genre, apiKey) {
+async function getBooks(url, genre) {
   const response = await fetch(
-    url + genre + "&langRestrict=en&printType=books&key=" + apiKey
+    url + genre + "&langRestrict=en" + "&printType=books"
   );
   const data = await response.json();
-  console.log(data.items);
   return data.items;
 }
 
@@ -16,7 +14,7 @@ async function cacheExist(genre) {
   if (cacheList) {
     return JSON.parse(cacheList);
   }
-  let bookDetails = await getBooks(url, genre, apiKey);
+  let bookDetails = await getBooks(url, genre);
   window.localStorage.setItem(listName, JSON.stringify(bookDetails));
   return bookDetails;
 }
@@ -25,16 +23,16 @@ function booksWrapTemplate(title, author, thumbnail) {
   return `<figure class="book-cover-container">
             <img class="book-cover" src=${thumbnail} alt="book cover">
             <div class="book-details">
-              <p>${title}</p>
-              <p>${author}</p>
-              <p>Price</p>
+              <p class="book-details__title">${title}</p>
+              <p class="book-details__author">${author}</p>
+              <p class="book-details__price">Price</p>
             </div>
           </figure>
           <button class="btn" type="button" name="button">Add to Cart</button>`;
 }
 
-async function renderBestSellers(id, genre) {
-  let bestSellersWrap = document.getElementById(id);
+async function renderBooksWrap(id, genre) {
+  let booksWrap = document.getElementById(id);
 
   const bookDetails = await cacheExist(genre);
   for (book in bookDetails) {
@@ -44,10 +42,10 @@ async function renderBestSellers(id, genre) {
     let bookItem = document.createElement("div");
     bookItem.className = "book-item";
     bookItem.innerHTML = booksWrapTemplate(title, author, thumbnail);
-    bestSellersWrap.appendChild(bookItem);
+    booksWrap.appendChild(bookItem);
   }
 }
 
-renderBestSellers("nonfiction", "nonfiction");
-renderBestSellers("fiction", "fiction");
-renderBestSellers("mystery", "mystery");
+renderBooksWrap("nonfiction", "nonfiction");
+renderBooksWrap("fiction", "fiction");
+renderBooksWrap("mystery", "mystery");
